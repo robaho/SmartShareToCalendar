9dff6304e7523e82ab24c7bb3fe76d12540c74ef
robaho <rengels@ix.netcom.com>
2018-09-13 12:00:30 -0500

    Initial commit

---
+++ src/com/robaho/smartsharecalendar/MyActivity.java
@@ -0,0 +1,31 @@
+package com.robaho.smartsharecalendar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.widget.TextView;
+
+/**
+ * this activity is only used to display the help when/if the application is launched,
+ * maybe eventually will allow some settings to be changed 
+ * 
+ * @author robertengels
+ */
+public class MyActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		TextView maintext = (TextView)findViewById(R.id.maintext);
+		maintext.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
